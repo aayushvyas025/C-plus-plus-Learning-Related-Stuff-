0cc7af52ecf3425afce7183bce52241abcbef198
Aayush Vyas <aayush25vyas@gmail.com>
2025-10-29 18:26:24 +0530

    Taking input and output in c++

---
+++ Striver - A to Z DSA Sheet/01.C++  Basics/02.Taking Input-Output in C++/program.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+
+using namespace std; 
+
+int main() { 
+      int numberOne, numberTwo;  // Here we do decelaration of number variable 
+      cout << "Enter Your  First Number " << endl; 
+      // Taking Input from User with the help of cin object which means "character input" 
+      cin >> numberOne;   
+      cout << "Enter Your Second Number" << endl; 
+      cin >> numberTwo; 
+      cout << "Value of First Number:" << numberOne << " and  of Second Number:" << numberTwo << '\n'; 
+    //  With the help of cin we can take input from user 
+    return 0; 
+}
