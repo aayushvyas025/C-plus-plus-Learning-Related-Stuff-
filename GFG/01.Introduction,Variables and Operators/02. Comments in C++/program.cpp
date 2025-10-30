17d4dc79619cd9104dead2cab6e8efe9045edd09
Aayush Vyas <aayush25vyas@gmail.com>
2025-10-30 20:51:32 +0530

    Comments in C++

---
+++ GFG/01.Introduction,Variables and Operators/02. Comments in C++/program.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std; 
+
+int main() {
+    //* Comments in C++ 
+    // Comments are the common feature which was provided by every programming language so it helps to create documents of our code or features so our code logic explain or understand to other developer when we are working in collaboration or with organization and also to take notes for ourself also.... 
+
+    //? Comments are of two types : 
+    //* 1st. Multi-line comments : In which we comment multi-lines for multi-line comment we are using backward slash and star for opening and closing /*  */ 
+    /* This  is Multiline  Comments */
+   //* We can comment our code also if we don't want to execute 
+   /*
+    cout << "Hello World" << "\n"; 
+    cout << "Helloooo World" << "\n"; 
+   */ 
+  //* 2nd. Single line Comments : In this single line comment we can comment only single line 
+  // This is single line comment 
+//   cout << "Helloooo World" << endl;    
+    return 0; 
+}
